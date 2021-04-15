ea38086fc413a35d896f95ad86e0622f3d42e3fb
Alain Deleglise <alain@icreo.fr>
2021-04-15 12:37:26 +0200

    Fixed postinstall

--- custom-postinstall.sh
+++ custom-postinstall.sh
@@ -1,5 +1,6 @@
 apt -y update  &&  apt upgrade -y && apt dist-upgrade -y
-apt remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
+apt -y remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+apt update
 apt -y install proxmox-ve
--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -5,7 +5,8 @@ chmod 700 /root/.ssh
 wget -O /root/.ssh/authorized_keys https://raw.github.com/adeleglise/hetzner-bootstrap/ax41-proxmox/root-ssh-authorizedkeys.pub
 bash < <(wget -qO - https://bit.ly/install-puppet-agent)
 apt -y update  &&  apt upgrade -y && apt dist-upgrade -y
-apt remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
+apt -y remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+apt update
 apt -y install proxmox-ve
