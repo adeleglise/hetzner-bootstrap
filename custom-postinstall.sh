2ffeb400fb50a9e03d725b54197b33e251b71a8f
Alain Deleglise <alain@icreo.fr>
2021-04-15 12:44:31 +0200

    Fixed proxmox postinstall

--- custom-postinstall.sh
+++ custom-postinstall.sh
@@ -1,6 +1,6 @@
-apt -y update  &&  apt upgrade -y && apt dist-upgrade -y
+apt -y update && apt upgrade -y && apt dist-upgrade -y
 apt -y remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
-apt update
-apt -y install proxmox-ve
+apt update && apt full-upgrade
+apt -y install proxmox-ve open-iscsi
