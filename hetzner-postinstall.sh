3f279378d0260bcdc9bb7baa57c3276f7624c33e
Alain Deleglise <alain@icreo.fr>
2021-04-15 13:26:32 +0200

    Added posttinstall answers

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -9,4 +9,9 @@ apt -y remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
 apt update && apt full-upgrade
+echo "samba-common samba-common/workgroup string  WORKGROUP" | debconf-set-selections
+echo "samba-common samba-common/dhcp boolean false" | debconf-set-selections
+echo "samba-common samba-common/do_debconf boolean true" | debconf-set-selections
+echo "postfix postfix/mailname string ${SERVERNAME}" | debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
 apt -y install proxmox-ve open-iscsi
