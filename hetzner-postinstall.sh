4a9a977e0c730491f96826229c2ad1dd69143b15
Alain Deleglise <alain@icreo.fr>
2021-08-12 13:57:59 +0200

    changed postinstall

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -3,11 +3,10 @@ apt-get -y install byobu wget
 mkdir /root/.ssh
 chmod 700 /root/.ssh
 wget -O /root/.ssh/authorized_keys https://raw.github.com/adeleglise/hetzner-bootstrap/ax41-proxmox/root-ssh-authorizedkeys.pub
-bash < <(wget -qO - https://bit.ly/install-puppet-agent)
 apt -y update && apt upgrade -y && apt dist-upgrade -y
 apt -y remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+wget http://download.proxmox.com/debian/proxmox-ve-release-7.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-7.x.gpg
 apt update && apt full-upgrade
 echo "samba-common samba-common/workgroup string  WORKGROUP" | debconf-set-selections
 echo "samba-common samba-common/dhcp boolean false" | debconf-set-selections
