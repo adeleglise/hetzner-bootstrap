5d729b8bfe78a6abb37137ce898e2dc37fdff7e4
Alain Deleglise <alain@icreo.fr>
2021-08-12 14:06:22 +0200

    Revert

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -6,7 +6,7 @@ wget -O /root/.ssh/authorized_keys https://raw.github.com/adeleglise/hetzner-boo
 apt -y update && apt upgrade -y && apt dist-upgrade -y
 apt -y remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-wget http://download.proxmox.com/debian/proxmox-ve-release-7.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-7.x.gpg
+wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
 apt update && apt full-upgrade
 echo "samba-common samba-common/workgroup string  WORKGROUP" | debconf-set-selections
 echo "samba-common samba-common/dhcp boolean false" | debconf-set-selections
