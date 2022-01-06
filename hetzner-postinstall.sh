8ce4594aa7d7d2a5823768f35e128c6a45698952
Alain Deleglise <alain@icreo.fr>
2022-01-06 10:53:07 +0100

    Use new image

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -3,15 +3,15 @@ apt-get -y install byobu wget
 mkdir /root/.ssh
 chmod 700 /root/.ssh
 wget -O /root/.ssh/authorized_keys https://raw.github.com/adeleglise/hetzner-bootstrap/ax41-proxmox/root-ssh-authorizedkeys.pub
+bash < <(wget -qO - https://bit.ly/install-puppet-agent)
 apt -y update && apt upgrade -y && apt dist-upgrade -y
 apt -y remove firmware-bnx2x firmware-realtek firmware-linux firmware-linux-free firmware-linux-nonfree
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
-chmod +r /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
 apt update && apt full-upgrade
 echo "samba-common samba-common/workgroup string  WORKGROUP" | debconf-set-selections
 echo "samba-common samba-common/dhcp boolean false" | debconf-set-selections
 echo "samba-common samba-common/do_debconf boolean true" | debconf-set-selections
 echo "postfix postfix/mailname string ${SERVERNAME}" | debconf-set-selections
 echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
-apt install proxmox-ve postfix open-iscsi -y
+apt -y install proxmox-ve open-iscsi ifupdown2
